c46a4ad73c23bc394ef803cc1bb13c943385ef98
Alvaro Navarro <alnacle@achtung.es>
2018-04-19 12:29:53 -0500

    Fix Flight most traveled destination sample

--- flight-most-traveled-destinations/flight-most-traveled-destinations.py
+++ flight-most-traveled-destinations/flight-most-traveled-destinations.py
@@ -6,7 +6,7 @@ try:
     '''
     Where were people flying to from Nice during the last August?
     '''
-    response = amadeus.travel.analytics.air_traffic.get(
+    response = amadeus.travel.analytics.air_traffic.traveled.get(
         origin='NCE', period='2017-08')
     print(response.data)
 
